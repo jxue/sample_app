1b833692b6409d055451b10b923815f124c95ab2
jxue <june.xue@calix.com>
2011-05-18 11:23:13 +0800

    Done Exercises 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
 
-  # Return a title on a per-page basis.
+  # Return logo picture
+  def logo
+    image_tag("littlegirl.jpg", :alt => "Sample App", :class => "round")
+  end 
+
+ # Return a title on a per-page basis.
   def title
     base_title = "Hey, I'm June :)"
     if @title.nil?
